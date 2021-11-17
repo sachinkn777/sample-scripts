3b6d2e08927f086b49ed750cf10b05d7619f3f9e
SACHIN KN <38110693+sachinkn777@users.noreply.github.com>
2021-11-17 13:16:04 +0530

    Update and rename script.sh to start.sh

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /opt/deeplearning/
-./install-driver.sh
---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install docker.io -y
+sudo docker pull nvcr.io/nvidia/clara-train-sdk:v4.0 &
+echo "0.0.0.0 flserver" >> /etc/hosts
+#Variables and Values
+BUCKET_NAME= "datacopy-bucket/provisioning"
+key_value= "0zOeNT3shZVjMmIK"
+#Pull the server.zip from the GCS Bucket and unzip
+gsutil -m cp -r gs://BUCKET_NAME/server.zip .
+unzip -oP key_value server.zip 
+''' cd startup/
+sudo su
+chmod 700 docker.sh start.sh && bash docker.sh
+docker exec -it flserver /bin/bash
+hostname '''
